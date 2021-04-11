49efd382b2656264eabe2a4344cbadb13aff08ec
Oisera <orinamark22@gmail.com>
2021-04-11 15:31:10 +0300

    added welcom function & add url path

--- meeting_planner/meeting_planner/urls.py
+++ meeting_planner/meeting_planner/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import  path
+
+from website.views import  welcome
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('welcome.html', welcome)
+
 ]
