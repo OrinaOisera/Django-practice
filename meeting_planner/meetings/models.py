5164ff744c4c4143a067a075836f2bde67ed4aeb
Oisera <orinamark22@gmail.com>
2021-04-12 12:26:43 +0300

    added deafault values

--- meeting_planner/meetings/models.py
+++ meeting_planner/meetings/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import time
 
 # Create your models here.
 
@@ -6,3 +7,5 @@ from django.db import models
 class Meeting(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
+    start_time = models.TimeField(default=time(9))
+    duration = models.IntegerField(default=1)
