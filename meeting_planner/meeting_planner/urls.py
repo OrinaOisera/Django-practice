f532e536d01f24f843d289f07255fdedd9577cbd
OrinaOisera <orinamark22@gmail.com>
2021-04-17 11:19:13 +0300

    created a urls in meetings app

--- meeting_planner/meeting_planner/urls.py
+++ meeting_planner/meeting_planner/urls.py
@@ -16,16 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from website.views import welcome, date, about
-from meetings.views import detail
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', welcome, name="home"),
     path('date', date),
     path('about', about),
-    path('meetings/<int:id>', detail, name="detail"),
+    path('meetings/', include('meetings.urls'))
 
 ]
--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404 , get_list_or_404
 from .models import Meeting
+from .models import Room
 
 # Create your views here.
 
@@ -8,3 +9,7 @@ def detail(request, id):
     # meeting = Meeting.objects.get(pk=id)
     meeting = get_object_or_404(Meeting, pk=id)
     return render(request, "meetings/detail.html", {"meeting": meeting})
+
+
+def rooms_list(request):
+    return render(request, "meetings/room_list.html", {"rooms": Room.objects.all()})
