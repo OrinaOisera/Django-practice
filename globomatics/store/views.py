7c7496964e160394fccb9263d4a886e75da70a5e
OrinaOisera <orinamark22@gmail.com>
2021-04-22 23:31:32 +0300

    added ways to handle post and get requests

--- globomatics/store/views.py
+++ globomatics/store/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import  HttpResponse
+from django.http import  HttpResponse , HttpResponseNotFound
 
 
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+
+
 def index(request):
     return HttpResponse("Hellllo ")
 
@@ -10,6 +13,13 @@ def index(request):
 def detail(request):
     return HttpResponse("Hello there globomatics is an ecommerce  store for all thingdselectronic")
 
-
+@csrf_exempt
 def electronics(request):
-    return HttpResponse("THis is the Made in Kenya Electronics section")
+    if request.method == "GET":
+        print(request.headers)
+        print("-----------------/n", request)
+        return HttpResponse("THis is the Made in Kenya Electronics section")
+    if request.method == 'POST':
+        print("This is not allowed")
+        return HttpResponseNotFound("This is not allowed")
+
