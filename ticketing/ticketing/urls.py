d598da7e129d0c091a86b8d5c86e036133bad893
OrinaOisera <orinamark22@gmail.com>
2021-07-03 12:06:34 +0300

    Adss ticketing

--- ticketing/ticketing/urls.py
+++ ticketing/ticketing/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path('submit', views.submit, name="submit"),
+    path('tickets', views.tickets, name="tickets"),
 ]
--- ticketing/ticketing/views.py
+++ ticketing/ticketing/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
-from django.shortcuts import  render
+from django.shortcuts import render
+from ticketing.models import Ticket
+import random
+import string
+
+
+def randomstring(stringlength=8):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringlength))
 
 
 def index(request):
@@ -7,4 +15,22 @@ def index(request):
 
 
 def submit(request):
+    # new_ticket = Ticket(submitter= randomstring(), body='Help m e with this bug')
+    # new_ticket.save()
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        body = request.POST.get('body')
+        new_ticket = Ticket(submitter=username, body=body)
+        new_ticket.save()
+        return HttpResponse ("Successfully submitted ticket")
     return render(request, 'submit.html')
+
+
+    return render(request, 'submit.html')
+
+
+def tickets(request):
+    all_tickets = Ticket.objects.all()
+    return render(request, 'tickets.html', {'tickets': all_tickets})
+
+
